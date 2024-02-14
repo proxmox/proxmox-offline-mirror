373a3ed3e0a4db4a631bf5b3f8f42a02ca8f77ff
Maximiliano Sandoval <m.sandoval@proxmox.com>
2024-02-14 10:27:30 +0100

    verifier: elide explicit lifetime
    Fixes the clippy lint:
    
    ```
    warning: the following explicit lifetimes could be elided: 'msg
      --> src/helpers/verifier.rs:93:32
       |
    93 | pub(crate) fn verify_signature<'msg>(
       |                                ^^^^
    94 |     msg: &'msg [u8],
       |           ^^^^
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#needless_lifetimes
       = note: `#[warn(clippy::needless_lifetimes)]` on by default
    help: elide the lifetimes
       |
    93 ~ pub(crate) fn verify_signature(
    94 ~     msg: &[u8],
       |
    ```
    
    Signed-off-by: Maximiliano Sandoval <m.sandoval@proxmox.com>

--- src/helpers/verifier.rs
+++ src/helpers/verifier.rs
@@ -90,8 +90,8 @@ impl<'a> VerificationHelper for Helper<'a> {
 }
 
 /// Verifies GPG-signed `msg` was signed by `key`, returning the verified data without signature.
-pub(crate) fn verify_signature<'msg>(
-    msg: &'msg [u8],
+pub(crate) fn verify_signature(
+    msg: &[u8],
     key: &[u8],
     detached_sig: Option<&[u8]>,
     weak_crypto: &WeakCryptoConfig,
